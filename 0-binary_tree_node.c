a799800cba1894d7527288bc21c051060c044942
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-10 09:50:56 -0500

    updating task 0 nd 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
-	new->n = value;
 	new->parent = parent;
+	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
 	return (new);
 }
