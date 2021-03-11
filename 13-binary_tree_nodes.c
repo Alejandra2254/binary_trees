e4ca0adf253b0f2fd657b33f93773381f227cd3d
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-11 12:31:31 -0500

    Task, counting nodes on a tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - nodes of a tree
+ * @tree: tree
+ * Return: nodes or 0 if is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t leavesT = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	leavesT = binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right);
+	
+	if (tree->left == NULL && tree->right == NULL)
+		return (leavesT);
+
+	return (leavesT + 1);
+}
