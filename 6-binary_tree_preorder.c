de91b7f3f6a7578594aafe3f3cd75c2a4382a426
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-11 08:59:56 -0500

    pre order task modified

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_preorder - order the tree
+ * @tree: tree to be organized
+ * @func: function to print
+ */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
 	{
 		return;
 	}
-	
+	if (!func)
+		return;
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
